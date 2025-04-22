81df576b079c17ee18112bc41bedddb706a0b2c4
Tushar9770 <tusharnegi243@gmail.com>
2025-04-22 08:50:52 -0400

    message

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -46,3 +46,8 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+let isAuthenticated=true;
+let authenticationStatus=isAuthenticated ? "authenticated" : "not authenticated";
+
+console.log("Authentication status:",authenticationStatus);
